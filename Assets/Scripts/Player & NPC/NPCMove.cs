87e345df5cad7ff4ff8a4e25fe830018570680b8
JLee <sssa011@naver.com>
2023-06-17 23:41:14 +0900

    플레이어, NPC 이동 기능 구현 및 사운드, 로비 UI 수정

---
+++ Assets/Scripts/Player & NPC/NPCMove.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// (위 + 아래, 좌 + 우) 랜덤 방향으로 패트롤 하기
+// 벽과 부딪히는 경우 계산
+public class NPCMove : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private float _waitTime = 1f; // in seconds
+    private float _waitCounter = 0f;
+    private int _currentWaypointIndex = 0;
+    private bool _waiting = false, _turn = false;
+    public bool isDead = false;
+
+    private SpriteRenderer spriteRenderer;
+    public List<Transform> waypoints;
+    Animator animator;
+
+    void Start()
+    {
+        animator = this.transform.GetChild(0).GetComponent<Animator>();
+        spriteRenderer = this.transform.GetChild(0).GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+
+        if (!isDead)
+        {
+            if (_waiting)
+            {
+                MoveAnimation(false);
+                _waitCounter += Time.deltaTime;
+                if (_waitCounter < _waitTime)
+                    return;
+                _waiting = false;
+                if (!_turn) { _turn = true; spriteRenderer.flipX = false; }
+                else if (_turn) { _turn = false; spriteRenderer.flipX = true; }
+            }
+            else
+            {
+                MoveAnimation(true);
+            }
+
+            Transform wp = waypoints[_currentWaypointIndex];
+            if (Vector3.Distance(transform.position, wp.position) < 0.01f)
+            {
+                transform.position = wp.position;
+                _waitCounter = 0f;
+                _waiting = true;
+
+                _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Count;
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    wp.position,
+                    speed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void MoveAnimation(bool value)
+    {
+        animator.SetBool("isMove", value);
+    }
+
+}
---
+++ Assets/Scripts/Player & NPC/PlayerMove.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    GameObject player;
+    Animator animator;
+    private SpriteRenderer spriteRenderer;
+    float playerSpeed = 0f;
+
+    private bool _isMove = false;
+
+    private void Start()
+    {
+        player = this.transform.gameObject;
+        animator = this.transform.GetChild(0).GetComponent<Animator>();
+        spriteRenderer = this.transform.GetChild(0).GetComponent<SpriteRenderer>(); 
+        playerSpeed = 2.0f;
+
+        Camera cam = Camera.main;
+        cam.transform.SetParent(transform);
+        cam.transform.localPosition = new Vector3(0f, 0f, -2.5f);
+        cam.orthographicSize = 1.5f;
+    }
+
+    private void FixedUpdate()
+    {
+        _isMove = false;
+
+        if (!Input.GetMouseButton(0))
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + playerSpeed * Time.deltaTime, transform.position.z);
+                _isMove = true;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position = new Vector3(transform.position.x - playerSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+                spriteRenderer.flipX = true;
+                _isMove = true;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y - playerSpeed * Time.deltaTime, transform.position.z);
+                _isMove = true;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position = new Vector3(transform.position.x + playerSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+                spriteRenderer.flipX = false;
+                _isMove = true;
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 dir = (Input.mousePosition - new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0f)).normalized;
+            transform.position += dir * playerSpeed * Time.deltaTime;
+            _isMove = dir.magnitude != 0f;
+
+        }
+
+        MoveAnimation(_isMove);
+
+    }
+
+    private void MoveAnimation(bool value)
+    {
+        animator.SetBool("isMove", value);
+    }
+}
